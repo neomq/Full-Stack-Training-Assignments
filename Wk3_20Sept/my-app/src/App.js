c6650d34a3144c367758f399febdc2c66dff66a7
Neo Min Qi <97939518+neomq@users.noreply.github.com>
2022-09-20 21:54:58 +0800

    created react app

---
+++ Wk3_20Sept/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">Hello World</h1>
+      <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  );
+}
+
+export default App;
