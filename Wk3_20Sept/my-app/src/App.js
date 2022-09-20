9cbddee9dfeb4da3f8980e0393add46dd92f106e
Neo Min Qi <97939518+neomq@users.noreply.github.com>
2022-09-20 23:07:39 +0800

    updated app.js

--- Wk3_20Sept/my-app/src/App.js
+++ Wk3_20Sept/my-app/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="title">Hello World</h1>
+      <p className="text">This is a new react app.</p>
       <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
   );
