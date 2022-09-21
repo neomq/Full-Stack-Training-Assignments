98830454e03587edc452206dc570b263e8c353af
Neo Min Qi <97939518+neomq@users.noreply.github.com>
2022-09-21 23:19:32 +0800

    initial commit

---
+++ Wk3_21Sept/my-app/src/components/Home.js
@@ -0,0 +1,14 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+function Home() {
+    return(
+        <div>
+            <Nav />
+            <h1>This is the home page.</h1>
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
---
+++ Wk3_21Sept/my-app/src/components/Nav.js
@@ -0,0 +1,9 @@
+function Nav() {
+    return(
+        <div>
+            <h1>This is the nav component.</h1>
+        </div>
+    )
+}
+
+export default Nav;
